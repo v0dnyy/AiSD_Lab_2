eae1af27c5a30d2484440e8c8dcab1496598f8a7
Степан Водянов <2020-01161@students.ssau.ru>
2022-04-25 02:12:19 +0400

    add bubble sort for int vector

--- Lab_2/Sort.cpp
+++ Lab_2/Sort.cpp
@@ -7,12 +7,12 @@ struct stats {
 };
 stats bubble_sort(std::vector<int>& data) {
     stats result;
-    for (size_t i = 0; i < data.size()-1; ++i) {
+    for (size_t i = 0; i < data.size() - 1; ++i) {
         for (size_t j = 0; j < data.size() - i - 1; ++j) {
             if (data[j] > data[j + 1]) {
                 result.comparison_count++;
                 int value = data[j + 1];
-                data[j+1] = data[j];
+                data[j + 1] = data[j];
                 data[j] = value;
                 result.copy_count++;
             }
