2f2fbecae03636a385c644e65a44e9900c114757
Степан Водянов <2020-01161@students.ssau.ru>
2022-05-16 15:31:24 +0400

    add merge for two sequences

--- Lab_2/Sort.cpp
+++ Lab_2/Sort.cpp
@@ -60,6 +60,82 @@ stats cocktail_sort(vector<int>& data){
     }
     return result;
 }
+stats Two_Way_Merge_Sort(std::vector<int>& data)
+{
+    stats stat;
+    std::vector<int> res(data.size());
+    int l = 0;
+    int r = data.size() - 1;
+    int count = 0;
+    while (l != r) {
+        ++count;
+        while (l < r && data[l] < data[l + 1]) ++l;
+        vector<int> leftpod(l + 1);
+        while (r > l && data[r] < data[r - 1]) --r;
+        vector<int> rightpod(data.size() - r);
+        for (int i = 0; i <= l; ++i) {
+            leftpod[i] = data[i];
+        }
+        for (int i = 0; i < data.size() - r; ++i) {
+            rightpod[i] = data[data.size() - 1 - i];
+        }
+        vector<int> respod(leftpod.size() + rightpod.size());
+        int j = 0;
+        int k = 0;
+        int c = 0;
+        /*for (int i = 0; i < leftpod.size() + rightpod.size(); i++) {
+            if (k < leftpod.size() && j < rightpod.size()) {
+                if (leftpod[j] > rightpod[k]) {
+                    respod[i]=rightpod[k];
+                    k++;
+                }
+                else {
+                    respod[i]=leftpod[j];
+                    j++;
+                }
+            }
+        }*/
+        for (int i = 0; i <= respod.size() - 1; i++) {
+            if (j <= leftpod.size() - 1 && leftpod[j] < rightpod[k]) {
+                respod[i] = leftpod[j];
+                ++j;
+            }
+            else {
+                respod[i] = rightpod[k];
+                if (k < rightpod.size() - 1) ++k;
+            }
+        }
+
+        /*else {
+            for (int i = j + k; i < leftpod.size(); i++) {
+                if (leftpod[j] <= rightpod[k]) {
+                    res[j + k] = leftpod[j];
+                    ++j;
+                }
+                else {
+                    res[j + k] = rightpod[k];
+                    ++k;
+                }
+            }
+            for (int i = leftpod.size(); i < respod.size(); i++) {
+                respod[respod.size() - i] = rightpod[i];
+            }
+        }
+    }*/
+        if (count % 2 != 0) {
+            for (int i = 0; i < respod.size(); ++i) {
+                res[i] = respod[i];
+            }
+        }
+        else {
+            for (int i = res.size() - 1; i >= res.size() - respod.size(); --i) {
+                res[i] = respod[res.size()-i];
+            }
+        }
+        data = res;
+    }
+    return stat;
+}
 int main() {
     vector<int> v1(5);
     stats result;
@@ -72,6 +148,7 @@ int main() {
     cout << "\n";
     //result=bubble_sort(v1);
     //result = cocktail_sort(v1);
+    result = Two_Way_Merge_Sort(v1);
     //cout << "Comparison count: "<<result.comparison_count<<endl;
     //cout << "Copy count: " << result.copy_count << endl;
     for (size_t i = 0; i < v1.size(); ++i) {
